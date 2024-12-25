550bbc8ec4cf7a50cee9a8b2d8105452cce735be
baharyilmazz13 <bahar.yilmaz2@std.yeditepe.edu.tr>
2024-12-25 21:12:46 +0300

    python

---
+++ iş alımı.py
@@ -0,0 +1,11 @@
+#SOFTWAREFİRMA
+note=int(input("Deine Note:"))
+if note>80:
+  print("Ja")
+else:
+  print("Nein")
+a=int(input("Deine Jahren:"))
+if 20<=a<=50:
+  print("Einstellen")
+else:
+  print("Ablehnen")
